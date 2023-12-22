700882792b5cb6facb56c4ae160e7ad040b02ec4
wala4 <wala4@op.pl>
2023-12-22 10:33:48 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace jenkins
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
